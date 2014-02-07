0700b87d07e65c058171f11c6bd47cd6496904d5
Levente Opelcz <levente@ayogo.com>
2014-02-06 16:19:16 -0800

    dodge text not spawning fix

--- js/jquery.dogeweather.js
+++ js/jquery.dogeweather.js
@@ -201,7 +201,7 @@
                 '<span style="position: absolute; left: ' + Math.random()  *100 + '%;top: ' + Math.random()  *100 + '%;font-size: ' + Math.max(20, (Math.random() * 50 + 24)) + 'px; color:' + randomFrom(suchcolors) + ';">'
                     + r(dogefix) +
                 '</span>');
-                var suchnumber = $("span").length;
+                var suchnumber = $(".such.overlay span").length;
                 if (suchnumber > 8 )
                 {
                     $('.such span:nth-child(1)').remove();
